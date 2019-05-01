c77e43ed64a54d46d3bc9be6b069fce82179e04b
1170500502 <libra.yang1024@gmail.com>
2019-05-01 19:04:39 +0800

    2rd Lab3Test

--- src/circularOrbit/CircularOrbitFactory.java
+++
@@ -1,8 +0,0 @@
-package circularOrbit;
-
-import java.io.IOException;
-
-public interface CircularOrbitFactory {
-	public CircularOrbit CreateAndLoad(String loadFrom) throws IOException;
-	public CircularOrbit Create(String type);
-}
